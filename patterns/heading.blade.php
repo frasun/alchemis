90f8392c9576d3f223408e5d8b4773019c19acbe
fraz <fraz@o2.pl>
2022-10-31 15:02:39 +0100

    add sample patterns

---
+++ patterns/heading.blade.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Heading with icon
+ * Slug: alchemis/heading-icon
+ * Categories: text
+ */
+?>
+<!-- wp:heading -->
+<h2>
+  <img class="wp-image-36 inline-block" style="width: 38px;"
+    src="http://alchemis.local/wp-content/uploads/2022/10/kropla-zielona.svg" alt="">
+  <span>qasddsdfsdf</span>
+</h2>
+<!-- /wp:heading -->
