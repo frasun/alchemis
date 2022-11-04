3c20486d7a2de3286a9b8e239a7b41ef0004bf41
fraz <fraz@o2.pl>
2022-11-04 12:51:53 +0100

    restyle footer

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,7 +1,7 @@
-<footer class="bg-dark text-white leading-tight pt-4 pb-5 bg-footer bg-no-repeat bg-bottom bg-[length:1000px]">
+<footer class="bg-dark text-white leading-tight pt-4 pb-5 bg-footer bg-no-repeat bg-bottom bg-[length:750px]">
   <div class="container">
-    <x-logo type="light" class="mb-3"></x-logo>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+    <x-logo type="light" class="mb-2"></x-logo>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
       @php(dynamic_sidebar('sidebar-footer'))
     </div>
 </footer>
