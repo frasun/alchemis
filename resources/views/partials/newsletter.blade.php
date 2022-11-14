875454be1ec7566516171768e9d6f527f2a11083
fraz <fraz@o2.pl>
2022-11-14 16:33:13 +0100

    fix line height in newsletter heading

--- resources/views/partials/newsletter.blade.php
+++ resources/views/partials/newsletter.blade.php
@@ -1,6 +1,6 @@
 <div class="container pt-5 pb-2">
   <header class="text-center">
-    <h3 class="text-xlg">dołącz do newslettera <br />i bądź na bieżąco</h3>
+    <h3 class="text-xlg leading-tight">dołącz do newslettera <br />i bądź na bieżąco</h3>
   </header>
   @include('forms.newsletter')
 </div>
