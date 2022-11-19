1c54b41a5034bb9da6365eb2dd37a24073fd7d1d
fraz <fraz@o2.pl>
2022-11-19 20:29:13 +0100

    disable notices in checkout

--- app/wc-template-hooks.php
+++ app/wc-template-hooks.php
@@ -309,12 +309,12 @@ remove_action('woocommerce_single_product_summary', 'woocommerce_template_single
  * Notices.
  */
 remove_action('woocommerce_cart_is_empty', 'woocommerce_output_all_notices', 5);
-// remove_action('woocommerce_shortcode_before_product_cat_loop', 'woocommerce_output_all_notices', 10);
+remove_action('woocommerce_shortcode_before_product_cat_loop', 'woocommerce_output_all_notices', 10);
 remove_action('woocommerce_before_shop_loop', 'woocommerce_output_all_notices', 10);
 remove_action('woocommerce_before_single_product', 'woocommerce_output_all_notices', 10);
 remove_action('woocommerce_before_cart', 'woocommerce_output_all_notices', 10);
-// remove_action('woocommerce_before_checkout_form_cart_notices', 'woocommerce_output_all_notices', 10);
-// remove_action('woocommerce_before_checkout_form', 'woocommerce_output_all_notices', 10);
+remove_action('woocommerce_before_checkout_form_cart_notices', 'woocommerce_output_all_notices', 10);
+remove_action('woocommerce_before_checkout_form', 'woocommerce_output_all_notices', 10);
 // remove_action('woocommerce_account_content', 'woocommerce_output_all_notices', 5);
 // remove_action('woocommerce_before_customer_login_form', 'woocommerce_output_all_notices', 10);
 // remove_action('woocommerce_before_lost_password_form', 'woocommerce_output_all_notices', 10);
