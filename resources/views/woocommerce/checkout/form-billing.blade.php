6ee3217c09d4e7355b091f0f4a92603cd9b1bb57
fraz <fraz@o2.pl>
2022-11-23 09:17:07 +0100

    fix create account checkbox in checkout

--- resources/views/woocommerce/checkout/form-billing.blade.php
+++ resources/views/woocommerce/checkout/form-billing.blade.php
@@ -49,12 +49,14 @@
 @if (!is_user_logged_in() && $checkout->is_registration_enabled())
   <section class="woocommerce-account-fields">
     @if (!$checkout->is_registration_required())
-      <p class="form-row form-row-wide create-account">
-        <label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
-          <input class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" id="createaccount"
-            <?php checked(true === $checkout->get_value('createaccount') || true === apply_filters('woocommerce_create_account_default_checked', false), true); ?> type="checkbox" name="createaccount" value="1" /> <span><?php esc_html_e('Create an account?', 'woocommerce'); ?></span>
+      <div class="pt-2">
+        <input id="createaccount"
+          {{ checked(true === $checkout->get_value('createaccount') || true === apply_filters('woocommerce_create_account_default_checked', false), true) }}
+          type="checkbox" name="createaccount" value="1" />
+        <label for="createaccount">
+          {!! esc_html_e('Create an account?', 'woocommerce') !!}
         </label>
-      </p>
+      </div>
     @endif
 
     @php
