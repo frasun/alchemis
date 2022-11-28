232c056e5b93c9300978dca71dc130db38d91bf5
fraz <fraz@o2.pl>
2022-11-28 09:40:28 +0100

    404

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -3,11 +3,11 @@
 @section('content')
   @include('partials.page-header')
 
-  @if (! have_posts())
-    <x-alert type="warning">
-      {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
-    </x-alert>
-
-    {!! get_search_form(false) !!}
+  @if (!have_posts())
+    <section class="container pb-5">
+      <x-alert type="warning">
+        {!! __('Sorry, but the page you are trying to view does not exist.', 'sage') !!}
+      </x-alert>
+    </section>
   @endif
 @endsection
