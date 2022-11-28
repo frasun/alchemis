6cbbbff955cf9b5ba8e942ff88bf3e6d7ba8e170
fraz <fraz@o2.pl>
2022-11-28 07:17:35 +0100

    add breadcrumbs to post

--- resources/views/partials/post-breadcrumbs.blade.php
+++ resources/views/partials/post-breadcrumbs.blade.php
@@ -1,5 +1,20 @@
+@php
+  if (has_category()) {
+      $post_categories = wp_get_post_categories($post->ID);
+      $category_id = $post_categories[0];
+  }
+@endphp
+
 <nav>
-  <a href="{!! get_post_type_archive_link('post') !!}">Blog</a>
+  <a href="{!! get_post_type_archive_link('post') !!}">{!! get_the_title(get_option('page_for_posts')) !!}</a>
   <span class="inline-block px-0.75 text-greyDark">></span>
+
+  @if (has_category())
+    <a href="{!! get_category_link($category_id) !!}">
+      {!! get_cat_name($category_id) !!}
+    </a>
+    <span class="inline-block px-0.75 text-greyDark">></span>
+  @endif
+
   <span>{!! $title !!}</span>
 </nav>
