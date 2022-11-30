cd69983ef8714c5b9f12be34f8df76921c94c16a
fraz <fraz@o2.pl>
2022-11-30 09:47:28 +0100

    add menu slot to footer

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,7 +1,15 @@
 <footer class="bg-dark text-white leading-tight pt-7 pb-9 bg-footer bg-no-repeat bg-bottom bg-[length:750px]">
   <div class="container">
     <x-logo type="light" class="mb-2"></x-logo>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-      @php(dynamic_sidebar('sidebar-footer'))
-    </div>
+    @if (is_active_sidebar('sidebar-footer') || has_nav_menu('primary_navigation'))
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+        @php(dynamic_sidebar('sidebar-footer'))
+
+        @if (has_nav_menu('primary_navigation'))
+          <nav id="footerMenu" aria-label="{{ wp_get_nav_menu_name('footer_navigation') }}">
+            {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'echo' => false]) !!}
+          </nav>
+        @endif
+      </div>
+    @endif
 </footer>
