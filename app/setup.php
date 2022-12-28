5ef41cb41cb848b610c253ba967164b04d800d34
fraz <fraz@o2.pl>
2022-12-28 13:51:26 +0100

    support sendinblue newsletter

--- app/setup.php
+++ app/setup.php
@@ -134,6 +134,11 @@ add_action('widgets_init', function () {
         'name' => __('Footer', 'sage'),
         'id' => 'sidebar-footer',
     ] + $config);
+
+    register_sidebar([
+        'name' => __('Newsletter', 'sage'),
+        'id' => 'sidebar-newsletter',
+    ] + $config);
 });
 
 
--- resources/views/partials/newsletter.blade.php
+++ resources/views/partials/newsletter.blade.php
@@ -1,6 +1,8 @@
-<aside class="container pt-5 pb-2">
+{{-- <aside class="container pt-5 pb-2">
   <header class="text-center">
     <h3 class="text-xlg leading-tight">dołącz do newslettera <br />i bądź na bieżąco</h3>
   </header>
   @include('forms.newsletter')
-</aside>
+</aside> --}}
+
+@php(dynamic_sidebar('sidebar-newsletter'))
