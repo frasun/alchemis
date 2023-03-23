7cffeccfe2f1c8a9e666b09090730a96b031c867
fraz <fraz@o2.pl>
2023-03-23 14:42:25 +0100

    remove pixel code from head

--- app/setup.php
+++ app/setup.php
@@ -330,32 +330,6 @@ if (!defined('WP_DEV')) :
             })(window, document, 'script', 'dataLayer', 'GTM-57N9F5N');
         </script>
         <!-- End Google Tag Manager -->
-        <!-- Meta Pixel Code -->
-        <script>
-            ! function(f, b, e, v, n, t, s) {
-                if (f.fbq) return;
-                n = f.fbq = function() {
-                    n.callMethod ?
-                        n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-                };
-                if (!f._fbq) f._fbq = n;
-                n.push = n;
-                n.loaded = !0;
-                n.version = '2.0';
-                n.queue = [];
-                t = b.createElement(e);
-                t.async = !0;
-                t.src = v;
-                s = b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t, s)
-            }(window, document, 'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1656380098135208');
-            fbq('track', 'PageView');
-        </script>
-        <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1656380098135208&ev=PageView&noscript=1" /></noscript>
-        <!-- End Meta Pixel Code -->
-        <meta name="facebook-domain-verification" content="ijmxcp3n5v2mmmklcimc2dfbcb6os7" />
     <?php
     });
 
